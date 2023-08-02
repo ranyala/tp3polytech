c104d336aca27b14d079c69c7bde03423922174c
Rania laouchem <ranialawchem@gmail.com>
2023-08-02 10:01:15 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TableauComponent } from './tableau/tableau.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TableauComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
